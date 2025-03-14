11aea3eb3ad45d0c5191a9d08e04f7d328091844
Bruno1809 <bfernando41@gmail.com>
2025-03-13 21:43:58 -0300

    Add files via upload

--- calcu.py
+++ calcu.py
@@ -2,66 +2,59 @@ executar = True
 while executar:
     escolhas = '''
             [1] ou [+] para Somar
-            [2] ou [-] para Subtrair
-            [3] ou [/] para Dividir
-            [4] ou [*] para Multiplicar
-            [5] para sair
+            [2] ou [+] para Subtrair
+            [3] ou [+] para Dividir
+            [4] ou [+] para Multiplicar
+            [5] para sair' 
             '''
     print(escolhas)
     operador = input("Qual sua opção?: ")
-
-    # Entrada dos números com conversão para float
-    try:
-        n1 = float(input("Qual o primeiro Valor? "))
-        n2 = float(input("Qual o segundo Valor? "))
-    except ValueError:
-        print("Por favor, insira valores numéricos válidos!")
-        continue
+    n1 = input("Qual o primeiro Valor? ")
+    n2 = input("Qual o segundo Valor? ")
+    n1 = input(n1)
+    n2 = input(n2)
 
     texto_sair = '''
             [1] Não, desejo sair!
-            [2] Sim, desejo realizar outro cálculo
+            [2] Sim, desejo realizar outro calculo
             '''
 
-    # Soma
-    if operador == "1" or operador == "+" or operador.lower() == "somar":
+    #Soma
+    if operador == "1" or operador == "+" or operador == "Somar":
         resultado = n1 + n2
         print("Resultado da soma: " + str(resultado)) 
         print(texto_sair)
-        operador = input("Deseja realizar outro cálculo? ")
+        operador = input("Desejar realizar outro calculo? ")
         if operador == "1":
             executar = False      
 
-    # Subtração
-    elif operador == "2" or operador == "-" or operador.lower() == "subtrair":
+    #Subtração
+    if operador == "2" or operador == "-" or operador == "Subtrair":
         resultado = n1 - n2
-        print("Resultado da subtração: " + str(resultado))
+        print("Resultado da subtração: " - str(resultado))
         print(texto_sair)
-        operador = input("Deseja realizar outro cálculo? ")
+        operador = input("Desejar realizar outro calculo? ")
+        if operador == "1":
+            executar = False
+    #Divisão
+    if operador == "3" or operador == "*" or operador == "Multiplicação":
+        resultado = n1 / n2
+        print("Resultado da Dividir: " - str(resultado))
+        print(texto_sair)
+        operador = input("Desejar realizar outro calculo? ")
         if operador == "1":
             executar = False
 
-    # Divisão
-    elif operador == "3" or operador == "/" or operador.lower() == "dividir":
-        if n2 == 0:
-            print("Não é possível dividir por zero!")
-        else:
-            resultado = n1 / n2
-            print("Resultado da divisão: " + str(resultado))
-        print(texto_sair)
-        operador = input("Deseja realizar outro cálculo? ")
-        if operador == "1":
-            executar = False
-
-    # Multiplicação
-    elif operador == "4" or operador == "*" or operador.lower() == "multiplicar":
+    #Multiplicação
+    if operador == "4" or operador == "/" or operador == "Dividir":
         resultado = n1 * n2
-        print("Resultado da multiplicação: " + str(resultado))
+        print("Resultado da Multiplicar: " - str(resultado))
         print(texto_sair)
-        operador = input("Deseja realizar outro cálculo? ")
+        operador = input("Desejar realizar outro calculo? ")
         if operador == "1":
             executar = False
 
-    # Sair
-    elif operador == "5" or operador.lower() == "sair":
+    #Sair
+    if operador =="5" or "sair":
+        print("Obrigado!")
         executar = False
